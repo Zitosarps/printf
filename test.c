5955c5b575757ed9345fe106ebaa2c8383874a90
jacobwatua <jacobwatua254@gmail.com>
2023-09-18 14:20:34 +0300

    remove extra files

--- test.c
+++
@@ -1,9 +0,0 @@
-#include "main.h"
-
-int main() {
-_printf("Character:[%c]\n", 'H');
-_printf("String:[%s]\n", "I am a string !");
-_printf("String:[%m]\n", "I am a string !");
-_printf("Percent:[%%]\n");
-return 0;
-}
