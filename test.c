b9dc6a7d8af18da6571946ed6dc3562e9c4fb285
jacobwatua <jacobwatua254@gmail.com>
2023-09-20 02:49:07 -0700

    attempt 10

--- test.c
+++
@@ -1,8 +0,0 @@
-#include "main.h"
-int main() {
-_printf("Character:[%c]\n", 'H');
-_printf("String:[%s]\n", "I am a string !");
-_printf("Percent:[%%]\n");
-_printf("Unknown:[%k]\n");
-return 0;
-}
