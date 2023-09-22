84a86d813b0b229cec3886818e373609308978ab
jacobwatua <jacobwatua254@gmail.com>
2023-09-21 23:37:30 -0700

    post refactor corrections-2

--- _print_character.c
+++ _print_character.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * _print_character - prints out character(s) to stdout
+ * put_character - prints out character(s) to stdout
  * @c: character to print
  *
  * Return: 1 on success else -1
